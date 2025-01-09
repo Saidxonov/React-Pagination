0774d54127a83289f3084f55bf988b3094520eca
Saidburxon <saidburxon@gmail.com>
2025-01-09 13:51:36 +0500

    changed JS

--- src/cursor.js
+++ src/cursor.js
@@ -6,10 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const posX = e.clientX;
     const posY = e.clientY;
 
-    // cursorDot.style.left = `${posX}px`;
-    // cursorDot.style.top = `${posY}px`;
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
 
-    cursorOutline.style.left = `${posX}px`;
-    cursorOutline.style.top = `${posY}px`;
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate(
+      {
+        left: `${posX}px`,
+        top: `${posY}px`,
+      },
+      { duration: 500, fill: "forwards" }
+    );
   });
 });
