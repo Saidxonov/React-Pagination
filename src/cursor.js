81589f742f65de24bc2fb07cfa7dc5895413ed40
Saidburxon <saidburxon@gmail.com>
2025-01-09 13:57:13 +0500

    added console.log for debugging

--- src/cursor.js
+++ src/cursor.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
     cursorDot.style.left = `${posX}px`;
     cursorDot.style.top = `${posY}px`;
 
+    console.log(posX);
+
     // cursorOutline.style.left = `${posX}px`;
     // cursorOutline.style.top = `${posY}px`;
 
