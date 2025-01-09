98436b796e43e255d896fcd1e6c339116653dcc9
Saidburxon <saidburxon@gmail.com>
2025-01-09 13:36:50 +0500

    changed custom cursor

--- src/cursor.js
+++ src/cursor.js
@@ -1,18 +1,18 @@
 const cursorDot = document.querySelector("[data-cursor-dot]");
 const cursorOutline = document.querySelector("[data-cursor-outline]");
 
-window.addEventListener("mousemove", function(e) {
-    const posX = e.clientX;
-    const posY = e.clientY;
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
 
-    cursorDot.style.left = `${posX}px`;
-    cursorDot.style.top = `${posY}px`;
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
 
-    // cursorOutline.style.left = `${posX}px`;
-    // cursorOutline.style.top = `${posY}px`;
+  cursorOutline.style.left = `${posX}px`;
+  cursorOutline.style.top = `${posY}px`;
 
-    cursorOutline.animate({
-        left: `${posX}px`,
-        top: `${posY}px`,
-    }, { duration: 500, fill: "forwards" });
+  // cursorOutline.animate({
+  //     left: `${posX}px`,
+  //     top: `${posY}px`,
+  // }, { duration: 500, fill: "forwards" });
 });
